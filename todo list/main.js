4f10797eba56144b3d836a3581085bbc96aa7a4a
kamyarrazi14999 <kami6535@gmail.com>
2025-07-15 11:28:15 +0330

    add initial settings for live server and implement todo input functionality

--- todo list/main.js
+++ todo list/main.js
@@ -1,1 +1,8 @@
+const addtodoButton = document.getElementById('.add-todo-btn');
+const addInput = document.getElementById('.add.input');
+const todos = [];
+addtodoButton.addEventListener('click', (e) => { 
+    e.preventDefault();
+    const todoText = addInput.value.trim();
 
+});
