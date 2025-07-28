91e3d306e8da29ba1dcc0f1b60e31551c2a260e7
kamyarrazi14999 <kami6535@gmail.com>
2025-07-28 14:57:07 +0330

    refactor: consolidate todo display logic into a dedicated function for improved readability

--- todo list/main.js
+++ todo list/main.js
@@ -3,35 +3,38 @@ const addInput = document.getElementById('add-input');
 const todoList = document.getElementById('todo-list');
 const todos = [];
 
+const Displaytodos = () => {
+    todoList.innerHTML = ''; // پاک کردن لیست قبل از افزودن آیتم‌ها
+    todos.forEach((todo, index) => { // اضافه کردن index
+        const todoItem = document.createElement('li');
+        todoItem.classList = 'todo-item';
+        todoItem.innerHTML = `
+            <span class="todo-text">${todo}</span>
+            <button type='button' class='todo-btn' id='edit-btn'>
+                <i class="fa fa-edit"></i>
+            </button>
+            <button type='button' class='todo-btn' id='delete-btn' data-index="${index}" onclick="deleteTodo(this)">
+                <i class='fa fa-trash'></i>
+            </button>
+        `;
+        todoList.appendChild(todoItem);
+    });
+};
+
 addtodoButton.addEventListener("click", (e) => {
     e.preventDefault();
     const value = addInput.value.trim();
-    // IF VALUE IS NOT EMPTY, ADD TO TODOS
-    // AND UPDATE THE LIST
     if (value !== '') {
         todos.push(value); // افزودن ورودی جدید به آرایه todos
         addInput.value = ''; // پاک کردن ورودی بعد از افزودن
-        todoList.innerHTML = ''; // پاک کردن لیست قبل از افزودن آیتم‌ها
-        todos.forEach((todo) => {
-            const todoItem = document.createElement('li');
-            todoItem.classList = 'todo-item';
-            todoItem.innerHTML = `
-                <span class="todo-text">${todo}</span>
-                <button type='button' class='todo-btn' id='edit-btn'>
-                    <i class="fa fa-edit"></i>
-                </button>
-                <button type='button' class='todo-btn' id='delete-btn' data-index=${index} onclick="deleteTodo(this)">
-                    <i class='fa fa-trash'></i>
-                </button>
-            `;
-            todoList.appendChild(todoItem);
-        });
+        Displaytodos(); // به‌روزرسانی نمایش لیست TODOs
     } else {
         alert('Please enter a todo item');
     }
 });
 
-const deleteTodo = (btn) => { 
+function deleteTodo(btn) {
     const index = btn.dataset.index;
-    
-} 
+    todos.splice(index, 1);   
+    Displaytodos(); // به‌روزرسانی نمایش لیست TODOs
+}
