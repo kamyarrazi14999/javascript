0d1d0954a421a048bc5a5e11d3d4765ff235d38a
kamyarrazi14999 <kami6535@gmail.com>
2025-08-26 00:48:25 +0330

    new

--- todo list/main.js
+++ todo list/main.js
@@ -1,31 +1,26 @@
 // Get required DOM elements
-const addtodoButton = document.getElementById('add-todo-btn');
-const addInput = document.getElementById('add-input');
-const todoList = document.getElementById('todo-list');
-const editformWrapper = document.getElementById('edit-form-wrapper');
-const editInput = document.getElementById('edit-input');
-const editForme = document.getElementById('edit-form');
-const aLerttext = document.querySelector('.alert-text');
-let todos;
-// Initialize todos array from local storage or as an empty array
-if(localStorage.getItem('todos')) {
-    todos = JSON.parse(localStorage.getItem('todos')); // Load todos from local storage
-} else {
-    todos = [];
-}
+const addtodoButton = document.getElementById("add-todo-btn");
+const addInput = document.getElementById("add-input");
+const todoList = document.getElementById("todo-list");
+const editformWrapper = document.getElementById("edit-form-wrapper");
+const editInput = document.getElementById("edit-input");
+const editForme = document.getElementById("edit-form");
+const aLerttext = document.querySelector(".alert-text");
+const todos = JSON.parse(localStorage.getItem("todos")) || []; // Initialize todos array from local storage or as empty array
 
 // save and load todos from local storage
 const saveTodosToLocalStorage = () => {
-    localStorage.setItem('todos',JSON.stringify(todos)); // Save todos array to local storage
+  localStorage.setItem("todos", JSON.stringify(todos)); // Save todos array to local storage
 };
 
 // Function to display all todos in the list
 const Displaytodos = () => {
-    todoList.innerHTML = ''; // Clear the list before adding new items
-    todos.forEach((todo, index) => { // Loop through todos array
-        const todoItem = document.createElement('li');
-        todoItem.classList = 'todo-item';
-        todoItem.innerHTML = `
+  todoList.innerHTML = ""; // Clear the list before adding new items
+  todos.forEach((todo, index) => {
+    // Loop through todos array
+    const todoItem = document.createElement("li");
+    todoItem.classList = "todo-item";
+    todoItem.innerHTML = `
             <span class="todo-text">${todo}</span>
             <button type='button' class='todo-btn' id='edit-btn' onclick="editTodo(this)" data-index="${index}">
                 <i class="fa fa-edit"></i>
@@ -34,80 +29,79 @@ const Displaytodos = () => {
                 <i class='fa fa-trash'></i>
             </button>
         `;
-        todoList.appendChild(todoItem); // Add item to the list
-        
-    });
-    // Check if there are no todos and display an alert message
-    if (todos.length === 0) {
-        aLerttext.classList.add('show-alert-text');
-        aLerttext.textContent = '😯 Empty list. Add a new todo to get started!';
-    };
+    todoList.appendChild(todoItem); // Add item to the list
+  });
+  // Check if there are no todos and display an alert message
+  if (todos.length === 0) {
+    aLerttext.classList.add("show-alert-text");
+    aLerttext.textContent = "😯 Empty list. Add a new todo to get started!";
+  }
 };
 Displaytodos(); // Initial call to display todos
 
 // Event listener for adding a new todo
 addtodoButton.addEventListener("click", (e) => {
-    e.preventDefault(); // Prevent default form submission
-    // Check if input is not empty
-    const value = addInput.value.trim();
-    if (value !== '') {
-        todos.push(value); // Add new todo to the array
-        saveTodosToLocalStorage(); // Save updated todos to local storage
-        Displaytodos(); // Update the displayed list
-        addInput.value = ''; // Clear input after adding
-        aLerttext.classList.remove('show-alert-text'); // Remove alert if present
-    } else {
-        // Show alert if input is empty
-        aLerttext.classList.add('show-alert-text');
-        aLerttext.textContent = '😯 Please enter a todo item';
-        aLerttext.classList.add('alert-animation');
-        aLerttext.addEventListener('animationend', () => {
-        // if animation ends, remove alert classes  
-            aLerttext.classList.remove('show-alert-text', 'alert-animation'); // Remove alert after animation
-        });
-     
-    }   
-    
+  e.preventDefault(); // Prevent default form submission
+  // Check if input is not empty
+  const value = addInput.value.trim();
+  if (value !== "") {
+    todos.push(value); // Add new todo to the array
+    saveTodosToLocalStorage(); // Save updated todos to local storage
+    Displaytodos(); // Update the displayed list
+    addInput.value = ""; // Clear input after adding
+    aLerttext.classList.remove("show-alert-text"); // Remove alert if present
+  } else {
+    // Show alert if input is empty
+    aLerttext.classList.add("show-alert-text");
+    aLerttext.textContent = "😯 Please enter a todo item";
+    aLerttext.classList.add("alert-animation");
+    aLerttext.addEventListener("animationend", () => {
+      // if animation ends, remove alert classes
+      aLerttext.classList.remove("show-alert-text", "alert-animation"); // Remove alert after animation
+    });
+  }
 });
 
 // Function to delete a todo item
 function deleteTodo(btn) {
-    const index = btn.dataset.index; // Get the index of the item to delete
-    todos.splice(index, 1);   // Remove item from array
-    Displaytodos(); // Update the displayed list
+  const index = btn.dataset.index; // Get the index of the item to delete
+  todos.splice(index, 1);
+  saveTodosToLocalStorage(); // Remove item from array
+  Displaytodos(); // Update the displayed list
 }
 
 // Function to edit a todo item
 function editTodo(btn) {
-    // Get the index of the item to edit
-    const index = btn.dataset.index;
+  // Get the index of the item to edit
+  const index = btn.dataset.index;
 
-    // Show the edit form and set its value
-    editformWrapper.classList.add('show-form');
-    editInput.value = todos[index];
-    editInput.dataset.index = index; // Store index in input dataset
-    editInput.focus(); 
+  // Show the edit form and set its value
+  editformWrapper.classList.add("show-form");
+  editInput.value = todos[index];
+  editInput.dataset.index = index; // Store index in input dataset
+  editInput.focus();
 
-    // Add event listener for submitting the edit form
-    editForme.addEventListener('submit', (e) => {
-        e.preventDefault(); // Prevent default form submission
-        // Check if input is not empty
-        if (editInput.value === '') {
-            aLerttext.classList.add('show-alert-text');
-            aLerttext.textContent = '😯 Please enter a todo item';
-            return;
-        }
-        editformWrapper.classList.remove('show-form'); // Hide edit form
-        const editingIndex = editInput.dataset.index; // Get index of item being edited
-        todos[editingIndex] = editInput.value; // Update item in array
-        Displaytodos(); // Update the displayed list
-    });
+  // Add event listener for submitting the edit form
+  editForme.addEventListener("submit", (e) => {
+    e.preventDefault(); // Prevent default form submission
+    // Check if input is not empty
+    if (editInput.value === "") {
+      aLerttext.classList.add("show-alert-text");
+      aLerttext.textContent = "😯 Please enter a todo item";
+      return;
+    }
+    editformWrapper.classList.remove("show-form"); // Hide edit form
+    const editingIndex = editInput.dataset.index; // Get index of item being edited
+    todos[editingIndex] = editInput.value; // Update item in array
+    saveTodosToLocalStorage(); // Save updated todos to edition
+    Displaytodos(); // Update the displayed list
+  });
 
-    // Close edit form when clicking outside of it
-    editformWrapper.addEventListener('click', (e) => {
-        if (e.target.getAttribute('id') === 'edit-form-wrapper') {
-            editformWrapper.classList.remove('show-form');
-            editInput.value = ''; // Clear input after closing form
-        }
-    });
+  // Close edit form when clicking outside of it
+  editformWrapper.addEventListener("click", (e) => {
+    if (e.target.getAttribute("id") === "edit-form-wrapper") {
+      editformWrapper.classList.remove("show-form");
+      editInput.value = ""; // Clear input after closing form
+    }
+  });
 }
