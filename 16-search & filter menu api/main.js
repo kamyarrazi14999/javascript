520e0719a7f84c57ab07bdf7ba30f5383052acf1
kamyarrazi14999 <kami6535@gmail.com>
2025-08-28 14:13:21 +0330

    Implement code changes to enhance functionality and improve performance

--- 16-search & filter menu api/main.js
+++ 16-search & filter menu api/main.js
@@ -1,1 +1,20 @@
-let menuItems = [];
+let menuItems = null;
+
+// Load menu items from API
+const fetchMenuItems = async () => {
+  // Make API request
+  const response = await axios.get(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s"
+  );
+    const data = await response.data;
+
+    menuItems = data.meals;
+    displayMenuItems(menuItems);
+};
+
+// Display menu items in the DOM
+const displayMenuItems = (items) => {
+
+}
+
+fetchMenuItems();
